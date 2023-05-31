96a80fb3b8f9e79da0f95cbbdc59c5b795b5f76f
luckyiso <iso121@yandex.ru>
2023-05-31 18:47:28 +1000

    Написал запросы к базе для получения, удаления, добавления задачи

---
+++ app/src/main/java/com/example/taskapp/Tasks/Data/DataSource/TaskDataBase.kt
@@ -0,0 +1,13 @@
+package com.example.taskapp.Tasks.Data.DataSource
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.taskapp.Tasks.Domain.Model.Task
+
+@Database(
+    entities = [Task::class],
+    version = 1
+)
+abstract class TaskDataBase: RoomDatabase(){
+    abstract val TaskDAO: TaskDAO
+}
--- app/src/main/java/com/example/taskapp/Tasks/Domain/Model/Task.kt
+++ app/src/main/java/com/example/taskapp/Tasks/Domain/Model/Task.kt
@@ -1,5 +1,14 @@
 package com.example.taskapp.Tasks.Domain.Model
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
-class Task {
-}
+data class Task (
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "Title") val Title: String?,
+    @ColumnInfo(name = "Description") val Desc: String?,
+    @ColumnInfo(name="SubTasks") val SubTask: String?,
+    @ColumnInfo(name="Favourite") val Favourite: Boolean,
+    @ColumnInfo(name ="Time") val Time: Long
+)
